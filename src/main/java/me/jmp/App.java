28a0cd383e8efd63d335cbff9530906b40b1eb4d
Joseph Peterson <supercrazysquid@gmail.com>
2026-05-08 14:21:51 -0400

    removed custom lib

--- src/main/java/me/jmp/App.java
+++ src/main/java/me/jmp/App.java
@@ -1,18 +1,9 @@
 package me.jmp;
 
-import com.almasb.fxgl.app.GameApplication;
-import com.almasb.fxgl.app.GameSettings;
-
-public class App extends GameApplication {
-	
-	@Override
-	protected void initSettings(GameSettings settings) {
-		settings.setWidth(800);
-		settings.setHeight(800);
-		settings.setTitle("Java Tower Defense");
-	}
+public class App {
 
 	public static void main(String[] args) {
-		launch(args);
+
 	}
+
 }
