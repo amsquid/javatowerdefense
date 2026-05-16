f0876b7673919ef0af14131f99bf617620a454d5
Joseph Peterson <supercrazysquid@gmail.com>
2026-05-16 15:51:26 -0400

    basic class setup

--- src/main/java/me/jmp/entities/Enemy.java
+++ src/main/java/me/jmp/entities/Enemy.java
@@ -1,6 +1,25 @@
 package me.jmp.entities;
 
-// TODO: Implement this
-public class Enemy {
+import me.jmp.math.Vector2;
 
+public abstract class Enemy {
+	protected Vector2 position;
+
+	public Enemy(Vector2 position) {
+		this.position = position;
+	}
+
+	public Enemy(float x, float y) {
+		this.position = new Vector2(x, y);
+	}
+
+	public Enemy() {
+		this.position = new Vector2(0, 0);
+	}
+
+	public abstract void move();
+
+	public Vector2 getPosition() {
+		return position;
+	}
 }
--- src/main/java/me/jmp/entities/Tower.java
+++ src/main/java/me/jmp/entities/Tower.java
@@ -1,5 +1,13 @@
 package me.jmp.entities;
 
-// TODO: Implement this
-public class Tower {
+import me.jmp.math.Vector2;
+
+public abstract class Tower {
+	private Vector2 position;
+
+	public abstract void shoot();
+
+	public Vector2 getPosition() {
+		return position;
+	}
 }
