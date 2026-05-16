e1ea9997d784a5a53db5bf8d99169350da76520a
Joseph Peterson <supercrazysquid@gmail.com>
2026-05-16 15:51:05 -0400

    Setup window for drawing

--- src/main/java/me/jmp/JavaTowerDefense.java
+++ src/main/java/me/jmp/JavaTowerDefense.java
@@ -2,8 +2,11 @@ package me.jmp;
 
 import me.jmp.entities.Enemy;
 import me.jmp.entities.Tower;
+import me.jmp.entities.enemies.TestEnemy;
 import me.jmp.math.Vector2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,35 +14,53 @@ import java.util.TimerTask;
 import javax.swing.JFrame;
 
 public class JavaTowerDefense {
-	private ArrayList<Enemy> enemies;
-	private ArrayList<Tower> towers;
-	private ArrayList<Vector2> path;
+	private static ArrayList<Enemy> enemies;
+	private static ArrayList<Tower> towers;
+	private static ArrayList<Vector2> path;
 
-	private JFrame jFrame;
+	private static Renderer renderer;
+
+	private static JFrame jFrame;
+
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 800;
 
 	public static void main(String[] args) {
-		new JavaTowerDefense().run();
-	}
-
-	public void run() {
 		enemies = new ArrayList<>();
 		towers = new ArrayList<>();
 
+		renderer = new Renderer();
+		renderer.setPreferredSize(new Dimension(WIDTH, HEIGHT)); //https://github.com/ImKennyYip/snake-java/blob/master/SnakeGame.java
+	
 		jFrame = new JFrame("Java Tower Defense");
-		jFrame.setSize(800, 800);
-		jFrame.add(new Renderer());
+		jFrame.add(renderer, BorderLayout.CENTER);
+		jFrame.setSize(WIDTH, HEIGHT);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setLocationRelativeTo(null);
+		jFrame.setResizable(false);
 		jFrame.setVisible(true);
+		jFrame.pack(); //https://github.com/ImKennyYip/snake-java/blob/master/App.java
+		renderer.requestFocus();
 
+		enemies.add(new TestEnemy(200.f, 200.f));
+	
 		//https://www.baeldung.com/java-timer-and-timertask
 		new Timer().schedule(new GameTimer(), 0, 10);
 	}
 
-	private class GameTimer extends TimerTask {
+	public static ArrayList<Enemy> getEnemies() {
+		return enemies;
+	}
+
+	public static ArrayList<Tower> getTowers() {
+		return towers;
+	}
+
+	private static class GameTimer extends TimerTask {
 		@Override
 		public void run() {
 			System.out.println("TICK");
-			jFrame.repaint();
+			renderer.repaint();
 		}
 	}
 }
