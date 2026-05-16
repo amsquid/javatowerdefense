1160dacf4026a5dbea247179d6576911b696f1b3
Joseph Peterson <supercrazysquid@gmail.com>
2026-05-16 15:51:32 -0400

    testing enemy

---
+++ src/main/java/me/jmp/entities/enemies/TestEnemy.java
@@ -0,0 +1,23 @@
+package me.jmp.entities.enemies;
+
+import me.jmp.entities.Enemy;
+import me.jmp.math.Vector2;
+
+public class TestEnemy extends Enemy {
+	public TestEnemy(Vector2 position) {
+		this.position = position;
+	}
+
+	public TestEnemy(float x, float y) {
+		this.position = new Vector2(x, y);
+	}
+
+	public TestEnemy() {
+		this.position = new Vector2(0, 0);
+	}
+
+	@Override
+	public void move() {
+
+	}
+}
