917742cd2126196e8f5a2887f78c048bf55ea001
Joseph Peterson <supercrazysquid@gmail.com>
2026-05-18 14:24:20 -0400

    redid test enemy class too

--- src/main/java/me/jmp/entities/enemies/TestEnemy.java
+++ src/main/java/me/jmp/entities/enemies/TestEnemy.java
@@ -1,19 +1,11 @@
 package me.jmp.entities.enemies;
 
 import me.jmp.entities.Enemy;
-import me.jmp.math.Vector2;
 
 public class TestEnemy extends Enemy {
-	public TestEnemy(Vector2 position) {
-		this.position = position;
-	}
-
-	public TestEnemy(float x, float y) {
-		this.position = new Vector2(x, y);
-	}
 
 	public TestEnemy() {
-		this.position = new Vector2(0, 0);
+		super(1, 0.1);
 	}
 
 	@Override
